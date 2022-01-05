6a630801e90e713d0e1f0297c2b68ed626852bf8
Philip Meulengracht <the_meulengracht@hotmail.com>
2022-01-05 23:05:51 +0100

    gracht: fix a typo in headers

--- include/rwlock.h
+++ include/rwlock.h
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Reader/Writer mutex implementation
  * It is a rather crude implementation of a read-write mutex that uses
--- runtime/queue.c
+++ runtime/queue.c
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Generic queue implementation, since these operations will be performed
  * during lock of a mutex, no sync is taken into account here.
