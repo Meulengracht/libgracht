77224451cc3b7460b43a8b32b56f4e0324bf6476
Philip Meulengracht <the_meulengracht@hotmail.com>
2022-07-29 12:31:15 +0200

    tests: update regex for windows

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,11 +3,11 @@
 # get all test programs that start with gclient in
 # the tests subfolder, they each end with a number, so
 # make sure they are sorted
-TESTS=$(find tests -name "*gclient*" -type f | sort -V)
+TESTS=$(find tests -regextype posix-extended -regex '.*/(gclient.*?exe|gclient[^.]*)' | sort -V)
 
 # get all server programs so we can test both single-threaded
 # and multi-threaded versions.
-SERVERS=$(find tests -name "gserver*" -type f)
+SERVERS=$(find tests -regextype posix-extended -regex '.*/(gserver.*?exe|gserver[^.]*)')
 
 # iterate servers, and then for each server we want to start
 # each test program
