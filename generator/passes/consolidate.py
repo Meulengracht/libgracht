4dc085465fcdaf4214df19f3f010ea457cf7def9
Philip Meulengracht <the_meulengracht@hotmail.com>
2026-03-23 14:00:33 +0100

    many: new control interface, refactor service grammar to v2 and introduce streams

--- generator/passes/consolidate.py
+++ generator/passes/consolidate.py
@@ -12,7 +12,7 @@ def consolidate_struct_member(service: ServiceObject, done: list, struct: Struct
         if struct_target is None:
             raise ValueError(f"Type {typename} cannot be resolved")
         if not struct_target in done:
-            consolidate_struct(struct_target)
+            consolidate_struct(service, done, struct_target)
         
         mems = struct.get_members()
         index = mems.index(member)
